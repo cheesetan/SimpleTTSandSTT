f8fa0976e19ad9996d92c93e1dc16cf7b2a82903
Tristan <github@tristanchay.com>
2025-11-08 05:44:00 +0800

    Initial Commit

---
+++ SimpleTTSandSTT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleTTSandSTT
+//
+//  Created by Tristan Chay on 8/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleTTSandSTT/SimpleTTSandSTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTTSandSTTApp.swift
+//  SimpleTTSandSTT
+//
+//  Created by Tristan Chay on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTTSandSTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
